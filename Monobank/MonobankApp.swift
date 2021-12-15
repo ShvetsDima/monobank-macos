70ef08bd1ec577d5fa9dae5636452a951da7cb39
Dima Shvets <aoedima@gmail.com>
2021-12-15 19:51:23 +0200

    Initial Commit

---
+++ Monobank/MonobankApp.swift
@@ -0,0 +1,17 @@
+//
+//  MonobankApp.swift
+//  Monobank
+//
+//  Created by Dima Shvets on 15.12.2021.
+//
+
+import SwiftUI
+
+@main
+struct MonobankApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
